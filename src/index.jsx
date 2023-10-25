6e9f0c1c742bed48191abfc3f33709b433234848
KevinSangregorio <kesangregorio@gmail.com>
2023-10-25 11:22:17 -0300

    feat: add routes and gitignore

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/* Dependencies */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+/* Styles */
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* Components */
+import App from "./App";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/dentist/:id",
+    element: <Detail />,
+  },
+  {
+    path: "/favs",
+    element: <Favs />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-      <App/>
-  </React.StrictMode>
+  <RouterProvider router={router}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RouterProvider>
 );
-
-
